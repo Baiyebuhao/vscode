ec63a5f9856e7dc96d7e4fe6c5f22c8a49d92330
Baiyebuhao <baiyebuhao@gmail.com>
2020-03-25 15:13:37 +0800

    2333

--- 执行力/季度报告/汇智信 抚宁 季度报告取数3.sql
+++ 执行力/季度报告/汇智信 抚宁 季度报告取数3.sql
@@ -33,3 +33,74 @@ from
 			 a2.id,
              a2.name) a5
              where a5.bankname = '秦皇岛抚宁家银村镇银行'
+
+
+---调查完成
+select * from 	
+(SELECT a4.name AS BankName,
+          CASE
+              WHEN a2.pro_type =1 THEN '信易贷'
+              WHEN a2.pro_type =2 THEN '信福时贷'
+              WHEN a2.pro_type =3 THEN '精细化'
+              WHEN a2.pro_type =4 THEN '银行产品'
+              ELSE '未归类'
+          END AS pro_type,		 
+       a2.id AS product_id,
+       a2.name AS product_name,
+	   count(DISTINCT CASE
+                             WHEN a3.research_status = '4' THEN a3.customer_id
+                         END) AS diaochaOver
+   
+   FROM warehouse_atomic_hzx_bank_product_info a2
+   JOIN warehouse_atomic_hzx_research_task AS a3 ON a2.id = a3.bank_pro_id
+   JOIN warehouse_atomic_hzx_b_bank_base_info AS a4 ON a3.bank_id = a4.id
+   where a2.id IS NOT NULL
+     and substr(a3.research_over_time,1,10) between '2020-01-01' and '2020-04-01'
+    GROUP BY a4.name,
+	         CASE
+                WHEN a2.pro_type =1 THEN '信易贷'
+                WHEN a2.pro_type =2 THEN '信福时贷'
+                WHEN a2.pro_type =3 THEN '精细化'
+                WHEN a2.pro_type =4 THEN '银行产品'
+                ELSE '未归类'
+             END,
+			 a2.id,
+             a2.name) a5
+             where a5.bankname = '秦皇岛抚宁家银村镇银行'
+
+---通过客户
+select * from 	                  --------sum(a5.diaochaOver)
+(SELECT a4.name AS BankName,
+          CASE
+              WHEN a2.pro_type =1 THEN '信易贷'
+              WHEN a2.pro_type =2 THEN '信福时贷'
+              WHEN a2.pro_type =3 THEN '精细化'
+              WHEN a2.pro_type =4 THEN '银行产品'
+              ELSE '未归类'
+          END AS pro_type,		 
+       a2.id AS product_id,
+       a2.name AS product_name,
+	   count(DISTINCT CASE
+                             WHEN a3.research_status = '4' THEN a3.customer_id
+                         END) AS diaochaOver,
+       sum(CASE 
+	            when a3.research_status = '4' THEN a3.rec_amount
+	            end) as dc_amount
+   FROM warehouse_atomic_hzx_bank_product_info a2
+   JOIN warehouse_atomic_hzx_research_task AS a3 ON a2.id = a3.bank_pro_id
+   JOIN warehouse_atomic_hzx_b_bank_base_info AS a4 ON a3.bank_id = a4.id
+   where a2.id IS NOT NULL
+     and substr(a3.research_over_time,1,10) between '2020-01-01' and '2020-04-01'
+     and a3.rec_amount > 0
+    GROUP BY a4.name,
+	         CASE
+                WHEN a2.pro_type =1 THEN '信易贷'
+                WHEN a2.pro_type =2 THEN '信福时贷'
+                WHEN a2.pro_type =3 THEN '精细化'
+                WHEN a2.pro_type =4 THEN '银行产品'
+                ELSE '未归类'
+             END,
+			 a2.id,
+             a2.name) a5
+             where a5.bankname = '秦皇岛抚宁家银村镇银行'
+
---
+++ 执行力/季度报告/汇智信 抚宁 季度报告取数4.sql
@@ -0,0 +1,50 @@
+--汇智信 抚宁 季度报告取数4
+
+--申请到分配
+select count(case when days=0 then 1 end ) as x1,
+        count(case when days>=1 and days<=2 then 1 end  ) as x23,
+        count(case when days>=3 and days<=5 then 1 end  ) as x35,
+        count(case when  days>5 or days is null then 1 end  ) as x5
+        from (
+select  datediff(substr(dis_time,1,10) ,
+                 substr(loan_apply_time,1,10)) as days
+from warehouse_atomic_hzx_research_task a1
+left join warehouse_atomic_hzx_b_bank_base_info a2
+on a1.bank_id = a2.id
+where a1.m_state='5'--营销完成
+  and substr(a1.loan_apply_time,1,10) between '2020-01-01' and '2020-04-01'
+  and a2.name = '秦皇岛抚宁家银村镇银行')x
+
+--分配到调查完成
+select count(case when days=0 then 1 end ) as x1,
+        count(case when days>=1 and days<=2 then 1 end  ) as x23,
+        count(case when days>=3 and days<=5 then 1 end  ) as x35,
+        count(case when  days>5 or days is null then 1 end  ) as x5
+        from (
+select datediff(substr(research_over_time,1,10) ,
+                 substr(dis_time,1,10)) as days                           -----research_apply_time
+from warehouse_atomic_hzx_research_task a1
+left join warehouse_atomic_hzx_b_bank_base_info a2
+on a1.bank_id = a2.id
+where a1.m_state='5'--营销完成
+  and a1.distribution = '1'  --已分配
+  and a1.research_status in ('2','3','4','5')--待调查2,调查中3,调查完成4,拒绝本次申请5'
+  and substr(a1.loan_apply_time,1,10) between '2020-01-01' and '2020-04-01'
+  and a2.name = '秦皇岛抚宁家银村镇银行')x
+  
+--调查到调查完成
+select count(case when days=0 then 1 end ) as x1,
+        count(case when days>=1 and days<=2 then 1 end  ) as x23,
+        count(case when days>=3 and days<=5 then 1 end  ) as x35,
+        count(case when  days>5 or days is null then 1 end  ) as x5
+        from (
+select datediff(substr(research_over_time,1,10) ,
+                 substr(research_apply_time,1,10)) as days                           -----research_apply_time
+from warehouse_atomic_hzx_research_task a1
+left join warehouse_atomic_hzx_b_bank_base_info a2
+on a1.bank_id = a2.id
+where a1.m_state='5'--营销完成
+  and a1.distribution = '1'  --已分配
+  and a1.research_status in ('2','3','4','5')--待调查2,调查中3,调查完成4,拒绝本次申请5'
+  and substr(a1.loan_apply_time,1,10) between '2020-01-01' and '2020-04-01'
+  and a2.name = '秦皇岛抚宁家银村镇银行')x
